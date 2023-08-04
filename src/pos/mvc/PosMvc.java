eead22a641099694db8d1c7374ce0d88e98603e8
sachinthadilshan <sachinthadilshan96@gmail.com>
2023-08-04 09:06:42 +0530

    customer add flow completed

--- src/pos/mvc/PosMvc.java
+++ src/pos/mvc/PosMvc.java
@@ -1,23 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package pos.mvc;
-import pos.mvc.view.CustomerView;
-
-/**
- *
- * @author sachinthadilshan
- */
-public class PosMvc {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-       new CustomerView().setVisible(true);
-    }
-    
-}
+/* * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template */package pos.mvc;import pos.mvc.view.CustomerView;/** * * @author sachinthadilshan */public class PosMvc {    /**     * @param args the command line arguments     */    public static void main(String[] args) {        // TODO code application logic here               new CustomerView().setVisible(true);    }    }
